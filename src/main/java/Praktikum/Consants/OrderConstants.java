b5ce535e9701c6727ada1c275653ea08e32c7076
Aleksahdr Daniliants <isken2046@yandex.ru>
2024-09-29 18:32:33 +0300

    Add OrderConstants.java

---
+++ src/main/java/Praktikum/Consants/OrderConstants.java
@@ -0,0 +1,49 @@
+package Praktikum.Consants;
+
+public class OrderConstants {
+
+    public static final String CREATE_ORDER_PATH = "/api/v1/orders";
+
+    public static final String CUSNOMER1_FIRST_NAME = "Кащей";
+    public static final String CUSNOMER2_FIRST_NAME = "Баба";
+    public static final String CUSNOMER3_FIRST_NAME = "Иван";
+    public static final String CUSNOMER4_FIRST_NAME = "Василиса";
+
+    public static final String CUSNOMER1_LAAST_NAME = "Бессмертный";
+    public static final String CUSNOMER2_LAAST_NAME = "Яга";
+    public static final String CUSNOMER3_LAAST_NAME = "Царевич";
+    public static final String CUSNOMER4_LAAST_NAME = "Премудрая";
+
+    public static final String CUSNOMER1_ADDRESS = "Кооперативная 2";
+    public static final String CUSNOMER2_ADDRESS = "Ткацкая 5";
+    public static final String CUSNOMER3_ADDRESS = "Дубнинская 2";
+    public static final String CUSNOMER4_ADDRESS = "Староконюшенный переулок";
+
+    public static final String CUSNOMER1_METROSTATION = "Лужники";
+    public static final String CUSNOMER2_METROSTATION = "Семеновская";
+    public static final String CUSNOMER3_METROSTATION = "Верхние Лихоборы";
+    public static final String CUSNOMER4_METROSTATION = "Кропоткинская";
+
+    public static final String CUSNOMER1_PHONE = "+79261112231";
+    public static final String CUSNOMER2_PHONE = "+79261112232";
+    public static final String CUSNOMER3_PHONE = "+79261112233";
+    public static final String CUSNOMER4_PHONE = "+79261112234";
+
+    public static final int CUSNOMER1_RENT_TIME = 1;
+    public static final int CUSNOMER2_RENT_TIME = 2;
+    public static final int CUSNOMER3_RENT_TIME = 3;
+    public static final int CUSNOMER4_RENT_TIME = 4;
+
+    public static final String CUSNOMER1_DELIVERY_DATE = "2024-10-01";
+    public static final String CUSNOMER2_DELIVERY_DATE = "2024-10-02";
+    public static final String CUSNOMER3_DELIVERY_DATE = "2024-10-03";
+    public static final String CUSNOMER4_DELIVERY_DATE = "2024-10-04";
+
+    public static final String CUSNOMER1_COMMENT = "Позвони мне, позвони.";
+    public static final String CUSNOMER2_COMMENT = "Жду не дождусь.";
+    public static final String CUSNOMER3_COMMENT = "Давай быстрее.";
+    public static final String CUSNOMER4_COMMENT = "Не звони сюда";
+
+    public static final String BLACK = "BLACK";
+    public static final String GREY = "GRAY";
+}
